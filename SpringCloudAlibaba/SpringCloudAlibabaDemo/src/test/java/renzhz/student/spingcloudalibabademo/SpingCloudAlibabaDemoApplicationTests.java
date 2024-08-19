16d894a0a4ec8333c5b06a18d4d8b7c200cabda3
Renzhz <73373245+Renzhz@users.noreply.github.com>
2024-08-19 22:03:28 +0800

    重构算法题，并初始化 SpringCloudAlibaba 项目

---
+++ SpringCloudAlibaba/SpringCloudAlibabaDemo/src/test/java/renzhz/student/spingcloudalibabademo/SpingCloudAlibabaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package renzhz.student.spingcloudalibabademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingCloudAlibabaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
